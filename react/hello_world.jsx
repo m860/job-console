f9c8421f262f826a81684c7ee317bee88c374547
jean <mahai_1986@126.com>
2015-11-13 16:45:48 +0800

    fixed

--- react/hello_world.jsx
+++ react/hello_world.jsx
@@ -1,10 +1,11 @@
 /**
  * Created by hai.ma on 2015/10/21 0021.
  */
-var React = require("react");
-var ReactDom = require("react-dom");
+//var React = require("react");
+//var ReactDom = require("react-dom");
+require("../react/main");
 
-module.exports = React.createClass({
+module.exports = global.React.createClass({
     displayName: "Hello",
     render: function () {
         return (
--- react/main.jsx
+++ react/main.jsx
@@ -1,6 +1,6 @@
 /**
  * Created by hai.ma on 2015/10/21 0021.
  */
-require("react");
-require("react-dom");
+global.React = require("react");
+global.ReactDom = require("react-dom");
 
