c7edc56c327b3f5635fb45cc3405422d083287a4
jean <mahai_1986@126.com>
2015-11-24 16:22:23 +0800

    fixed

--- jobs/job_stock.js
+++ jobs/job_stock.js
@@ -1,15 +1,30 @@
-var second = [];
-for (var i = 1; i < 60; i++) {
-    second.push(i);
+var finance = require("yahoo-finance");
+var q = require("q");
+
+function fetchData(ops) {
+    var deferred = q.defer();
+    finance.historical(ops, function (err, quotes) {
+        if (err) {
+            deferred.reject(err);
+        }
+        else {
+            deferred.resolve(quotes);
+        }
+    });
+    return deferred.promise;
 }
 
+
 module.exports = {
-    name: "job_demo",
-    rule: {
-        second: second
-    },
+    name: "fetch stock data",
     callback: function () {
-        console.log(this.name, new Date());
+        return fetchData({
+            symbol: 'AAPL',
+            from: '2012-01-01',
+            to: '2012-12-31'
+        }).then(function(quotes){
+            console.log(quotes);
+        });
     },
-    desc:"It's a test job ."
+    desc: "fetch stock data"
 };
