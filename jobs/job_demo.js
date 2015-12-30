9822d51a0b795bf095a4663463b6b4e0991d10c5
jean <mahai_1986@126.com>
2015-12-30 14:17:57 +0800

    fixed

--- jobs/job_demo.js
+++ jobs/job_demo.js
@@ -4,12 +4,22 @@ for (var i = 1; i < 60; i++) {
 }
 
 module.exports = {
+    //Job name
+    //optional
     name: "job_demo",
+    //job time trigger
+    //you my be consult https://github.com/node-schedule/node-schedule
+    //optional
+    //if it is null or undefined role is FUN else JOB
     rule: {
         second: second
     },
+    //do something
+    //require
     callback: function () {
         console.log(this.name, new Date());
     },
+    //description
+    //optional
     desc:"It's a test job ."
 };
